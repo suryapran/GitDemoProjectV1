35cb034f5d593f4a8c77b1d0ba93f608270888d1
surya <suryagayathri2002@yahoo.com>
2023-07-29 00:10:12 -0400

    commit local  my branch

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.print("my first commit tp2 surya");
+		System.out.print("my first commit tp2 surya");
+		System.out.print("my first commit tp2 surya");
+		System.out.print("my first commit tp2 surya");
+
+	}
+
+}
